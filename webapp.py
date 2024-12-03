99d4b3659aa553e97de6c6b83dbe3f589f3ff0cf
EvanderGibson <evanderbg2@gmail.com>
2024-12-03 13:55:20 -0800

    hi

--- webapp.py
+++ webapp.py
@@ -8,11 +8,15 @@ import pprint
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
-def render_main():
-    connection_string = os.environ["MONGO_CONNECTION_STRING"]
-    db_name = os.environ["MONGO_DBNAME"]
-    
-    client = pymongo.MongoClient(connection_string)
-    db = client[db_name]
-    collection = db['Books']
-    return render_template('home.html')
+def render_home():
+   # connection_string = os.environ["MONGO_CONNECTION_STRING"]
+   # db_name = os.environ["MONGO_DBNAME"]
+   # 
+   # client = pymongo.MongoClient(connection_string)
+   # db = client[db_name]
+   # collection = db['Books']
+   return render_template('home.html')
+
+
+if __name__=="__main__":
+    app.run(debug=True)

24b99ab385814d755971d499574e5543efeea9fe
Owen <owennsullivan@icloud.com>
2024-12-04 12:35:18 -0800

    Delete env.bat

--- env.bat
+++
@@ -1,6 +0,0 @@
-mongodb+srv://User0:V7YkAwEKlczLHKsG@cluster0.iali3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-set "MONGO_DBNAME=Cluster0"
-set GITHUB_CLIENT_ID=Ov23liKpppAT79QRsyiw
-set GITHUB_SECRET=7c3e94af37b07778f0563a7da4a8adf36af7cb35
-set SECRET_KEY=KEY
-python app.py
